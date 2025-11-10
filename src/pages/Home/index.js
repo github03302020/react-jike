b9d7679b0945283dd8e5c62e94d0f2656432d6ae
SunBing <cbsunbing@126.com>
2025-11-10 10:18:59 +0800

    echarts图标的初始化

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,38 @@
+import * as echarts from 'echarts';
+import { useEffect, useRef} from 'react';
+
+const option = {
+  xAxis: {
+    type: 'category',
+    data: ['vue','react','angular']
+  },
+  yAxis: {
+    type: 'value'
+  },
+  series: [
+    {
+      data: [40, 70, 20],
+      type: 'bar'
+    }
+  ]
+};
+
+
 const Home = () => {
-  return <div> this is Home </div>
+  const chartRef = useRef(null)
+  
+  useEffect(()=>{
+    // const chartDom = document.getElementById('main');
+    const chartDom = chartRef.current
+    const myChart = echarts.init(chartDom);
+    option && myChart.setOption(option);
+  },[])
+
+  return (
+    <>
+      <div id='main' ref={chartRef} style={{width:'500px', height:'400px'}}> </div>
+    </>
+  )
 }
 
 export default Home
