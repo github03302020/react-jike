2dbea26bce16e45df8d3069d48df38a9879e9db3
SunBing <cbsunbing@126.com>
2025-11-12 19:09:39 +0800

    携带封面图发布文章

--- src/pages/Publish/index.js
+++ src/pages/Publish/index.js
@@ -1,12 +1,14 @@
-import { Card, Breadcrumb, Form, Input, Select, Button } from 'antd'
+import { Card, Breadcrumb, Form, Input, Select, Button, Radio, Upload, message } from 'antd'
+import { PlusOutlined } from '@ant-design/icons'
 import { Link } from 'react-router-dom'
 import ReactQuill from 'react-quill-new'
 import 'react-quill-new/dist/quill.snow.css'
 import './index.scss'
 import { useEffect, useState } from 'react'
 import { createArticleAPI, getChannelsAPI } from '@/apis/article'
+import { type } from '@testing-library/user-event/dist/type'
 
-const { Option } =Select
+// const { Option } =Select
 const Publish = () => {
   const [channels, setChannels] = useState([])
   useEffect(()=>{
@@ -17,24 +19,36 @@ const Publish = () => {
     getChannels()
   },[])
   console.log(channels)
-  // const options = channels.map(item => ({ label: item.name, value: item.id }))
-  // console.log(options)
+  const options = channels.map(item => ({ label: item.name, value: item.id }))
+  console.log(options)
+  
+  const [messageApi, contextHolder] = message.useMessage()
 
   const onFinish = (value)=>{
     console.log(value)
-    const {title, channel, content } = value
+    const {title, channel, content, type } = value
+    if (imageList.length !== type) return messageApi.warning('封面类型和图片数量不匹配')
     const data = {
       title,
       content,
       cover:{
-        type:0,
-        images:[]
-      },
+        type,
+        images: imageList.map(item => item.thumbUrl),
+        },
       channel_id: channel
     }
     createArticleAPI(data)
   }
+
+  const [imageList, setImageList]=useState([])
+  const handleChange = (value)=>{
+    console.log('正在上传',value)
+    setImageList(value.fileList)
+  }
+  const [selectValue, setSelectValue] = useState(0)
+
   return (<div> 
+    {contextHolder}
     <Card title={
       <Breadcrumb
         items={[
@@ -52,6 +66,7 @@ const Publish = () => {
         wrapperCol={{ span: 10 }}
         autoComplete="off"
         onFinish={onFinish}
+        initialValues={{type:0}}
       >
         <Form.Item
           label="标题"
@@ -68,16 +83,43 @@ const Publish = () => {
           <Select
             allowClear
             placeholder="请选择文章频道"
-            // options ={options} 
-            // options={[
-            //   { label: 'male', value: 'male' },
-            //   { label: 'female', value: 'female' },
-            //   { label: 'other', value: 'other' },
-            // ]}
+            options ={options} 
           >
-           {channels.map(item=><Option key={item.id} value={item.id}>{item.name}</Option>)}
+           {/* {channels.map(item=><Option key={item.id} value={item.id}>{item.name}</Option>)} */}
           </Select>
         </Form.Item>
+
+        <Form.Item
+          label="封面"
+          name="type"
+        >
+          <Radio.Group
+            options={[
+              { value: 1, label: '单图' },
+              { value: 3, label: '三图' },
+              { value: 0, label: '无图' },
+            ]}
+            onChange={(e)=>setSelectValue(e.target.value)}
+          />
+        </Form.Item>
+        {selectValue>0 && <Form.Item
+          label={null}
+          name="cover"
+        >
+            <Upload
+              name="image"
+              listType="picture-card"
+              showUploadList={true}
+              maxCount={selectValue}
+              // action="http://localhost:3004/upload"
+              onChange={handleChange}
+              beforeUpload={()=>false}
+            >
+              <div>
+                <PlusOutlined />
+              </div>
+            </Upload>
+        </Form.Item>}
 
         <Form.Item
           label="内容"
